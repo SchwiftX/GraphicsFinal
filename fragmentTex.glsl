432549a1a6b091892d14de06c53b2c9c8f19f0f1
lzk981854172 <34226936+lzk981854172@users.noreply.github.com>
2017-12-18 08:09:11 +0800

    trans
    trans

--- fragmentTex.glsl
+++ fragmentTex.glsl
@@ -10,25 +10,17 @@ out vec4 outColor;
 
 uniform sampler2D tex0;
 uniform sampler2D tex1;
+
 uniform int texID;
-uniform float u_numShades; // number of shades
+uniform float alpha;
+
+uniform struct PointLight{
+    vec3 position;
+    vec3 intensity;
+}pl1,pl2;
 
 
 const float ambient = .3;
-
-// calculate diffuse component of lighting
-float diffuseSimple(vec3 L, vec3 N){
-   return clamp(dot(L,N),0.0,1.0);
-}
-
-// calculate specular component of lighting
-float specularSimple(vec3 L,vec3 N,vec3 R){
-   if(dot(N,L)>0){
-      return pow(clamp(dot(L,R),0.0,1.0),4.0);
-   }
-   return 0.0;
-}
-
 void main() {
    vec3 color;
    if (texID == -1)
@@ -36,20 +28,35 @@ void main() {
    else if (texID == 0)
      color = texture(tex0, texcoord).rgb;
    else if (texID == 1)
-     color = texture(tex1, texcoord).rgb;  
+     color = texture(tex1, texcoord).rgb;
    else{
    	 outColor = vec4(1,0,0,1);
    	 return; //This was an error, stop lighting!
    	}
-   vec3 viewDir = normalize(-pos); //We know the eye is at (0,0)!
-   vec3 reflectOfViewDir = reflect(viewDir,normal);
+
+    //add the diffuse/specular color with point light
+   vec3 Pl1direction = pl1.position-pos;
+   vec3 Pl2direction = pl2.position-pos;
+   float length1 = length(Pl1direction);
+   float length2 = length(Pl2direction);
+   Pl1direction = normalize(Pl1direction);
+   Pl2direction = normalize(Pl1direction);
+   vec3 diffuseP1 = color*pl1.intensity/length1*max(dot(Pl1direction,normal),0.0);
+   vec3 diffuseP2 = color*pl2.intensity/length2*max(dot(Pl2direction,normal),0.0);
 
    vec3 diffuseC = color*max(dot(-lightDir,normal),0.0);
-   float idiff = diffuseSimple(-lightDir,normal);
-   float ispec = specularSimple(-lightDir,normal,-reflectOfViewDir);
-   float intensity = ambient + idiff + ispec;
-   // quantize intensity for cel shading
-   float shadeIntensity = ceil(intensity * u_numShades)/ u_numShades;
-
-   outColor = vec4(color*shadeIntensity,1);
+   vec3 ambC = color*ambient;
+   vec3 viewDir = normalize(vec3(2.5,7,2.5)-pos); //We know the eye is at (0,0)!
+   vec3 reflectDir = reflect(viewDir,normal);
+   float spec = max(dot(reflectDir,lightDir),0.0);
+   float spec2 = max(dot(reflectDir,Pl1direction),0.0);
+   float spec3 = max(dot(reflectDir,Pl2direction),0.0);
+   if (dot(-lightDir,normal) <= 0.0)spec = 0;
+   if (dot(-Pl1direction,normal) <= 0.0)spec2 = 0;
+   if (dot(-Pl2direction,normal) <= 0.0)spec3 = 0;
+   vec3 specC = .8*vec3(1.0,1.0,1.0)*pow(spec,4);
+   vec3 specP2 = .2*vec3(1.0,1.0,1.0)*pow(spec2,4);
+   vec3 specP3 = .2*vec3(1.0,1.0,1.0)*pow(spec3,4);
+   vec3 oColor = ambC+diffuseC+diffuseP1+diffuseP2+specC+specP2+specP3;
+   outColor = vec4(oColor,alpha);
 }
